f55b52509f3b8de77fb5ea3d7737ccb4aec808da
Jack Works <zjwpeter@gmail.com>
2019-09-07 11:19:30 +0800

    feat: make the ServiceWorker general

--- service-worker.js
+++ service-worker.js
@@ -3,19 +3,47 @@
 
 addEventListener('fetch', e => {
     const url = new URL(e.request.url)
-    if (url.pathname === '/css-module-loader.js') {
-        const request = fetch(url.searchParams.get('src'))
-        const css = request
-            .then(x => x.text())
-            .then(x => {
+    for (const loader of Loader.loaders) {
+        if (url.pathname === loader.fallbackURL) {
+            const request = fetch(url.searchParams.get('src'))
+            const translated = request.then(loader.transpiler).then(file => {
                 const headers = new Headers(request.headers)
-                const cssModule = `let s = new CSSStyleSheet()
-s.replace(${JSON.stringify(x)})
-export default s`
                 headers.set('content-type', 'application/javascript')
-                headers.set('content-length', cssModule.length)
-                return new Response(cssModule, { headers })
+                headers.set('content-length', file.length)
+                return new Response(file, { headers })
             })
-        e.respondWith(css)
+            e.respondWith(translated)
+        }
     }
 })
+
+class Loader {
+    /**
+     * @param {string} fallbackURL The fallback URL
+     * @param {(res: Response) => Promise<string>} transpiler The transpiler
+     */
+    constructor(fallbackURL, transpiler) {
+        this.fallbackURL = fallbackURL
+        this.transpiler = transpiler
+    }
+    /**
+     * @type {Loader[]}
+     */
+    static loaders = []
+    /**
+     * @param {Loader} loader
+     */
+    static add(loader) {
+        this.loaders.push(loader)
+    }
+}
+
+Loader.add(
+    new Loader(
+        '/css-module-loader.js',
+        async res =>
+            `const container = new CSSStyleSheet()
+container.replace(${JSON.stringify(await res.text())})
+export default container`
+    )
+)
